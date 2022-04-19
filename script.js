aa026239acef2bf8f9be4fdc340c7b0a39950afc
JinPengNeymar <103395414+JinPengNeymar@users.noreply.github.com>
2022-04-19 11:54:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+ function total(){
+ var aa =document. getElementById ('aa').value; var        bb=document. getElementById ('bb').value;
+var total=Number(aa)+ Number(bb);
+ document.getElementById ('result').innerHTML=total;
+}
+ </script>
