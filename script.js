9e49ae9f5bb9b5e4c633697ac1915ab7e23368cd
JinPengNeymar <103395414+JinPengNeymar@users.noreply.github.com>
2022-04-19 12:01:39 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-<script>
- function total(){
- var aa =document. getElementById ('aa').value; var        bb=document. getElementById ('bb').value;
-var total=Number(aa)+ Number(bb);
- document.getElementById ('result').innerHTML=total;
-}
- </script>
